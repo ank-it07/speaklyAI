05bf01d8214207f8ea801a6013eb38a2dba76cfc
ank-it07 <kumarankit908011@gmail.com>
2024-10-27 00:56:31 +0530

    Uploadthing db post fix3

--- lib/db.ts
+++ lib/db.ts
@@ -1,15 +1,26 @@
+// import { neon } from "@neondatabase/serverless";
+
+// export default async function getDbConnection() {
+//   if (!process.env.DATABASE_URL) {
+//     throw new Error("Neon Database URL is not defined");
+//   }
+//   try {
+//     const sql = neon(process.env.DATABASE_URL);
+//     console.log("Database connection established");
+//     return sql;
+//   } catch (error) {
+//     console.error("Database connection error:", error);
+//     throw error;
+//   }
+// }
+
+
 import { neon } from "@neondatabase/serverless";
 
 export default async function getDbConnection() {
   if (!process.env.DATABASE_URL) {
     throw new Error("Neon Database URL is not defined");
   }
-  try {
-    const sql = neon(process.env.DATABASE_URL);
-    console.log("Database connection established");
-    return sql;
-  } catch (error) {
-    console.error("Database connection error:", error);
-    throw error;
-  }
+  const sql = neon(process.env.DATABASE_URL);
+  return sql;
 }
